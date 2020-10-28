e010dbe8c734ff9de184caeac37ff1f9506eacf9
yama <devthinkrec@gmail.com>
2020-10-28 10:03:43 +0900

    [change] テキストを変えてみた

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -10,14 +10,11 @@ export default function Home({ allPostsData }) {
           <title>{siteTitle}</title>
         </Head>
           <section className={utilStyles.headingMd}>
+              <p>tech storage</p>
               <p>ama</p>
-              <p>
-                  (This is a sample website - you’ll be building a site like this on{' '}
-                  <a href="https://nextjs.org/learn">our Next.js tutorial</a>
-              </p>
           </section>
           <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <h2 className={utilStyles.headingLg}>tech storage Blog</h2>
               <ul className={utilStyles.list}>
                   {allPostsData.map(({ id, date, title }) => (
                       <li className={utilStyles.listItem} key={id}>
